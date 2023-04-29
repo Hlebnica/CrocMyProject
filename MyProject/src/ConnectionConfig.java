9ee2c9bc4f339aebb4a848c018547e98db16ca38
Hlebnica <apple260500@gmail.com>
2023-04-29 21:31:51 +0500

    test

---
+++ MyProject/src/ConnectionConfig.java
@@ -0,0 +1,6 @@
+/** Глобальная переменная с путем до места хранения БД
+ *
+ */
+public class ConnectionConfig {
+  public static final String URL = "jdbc:h2:tcp://localhost:9092/E:/course3sem6/CrocMyProject/MyProject/db/movies";
+}
--- MyProject/src/Main.java
+++ MyProject/src/Main.java
@@ -1,15 +1,24 @@
 import java.sql.*;
 
-
 public class Main {
 
   public static void main(String[] args) {
-    String url = "jdbc:h2:tcp://localhost:9092/E:/course3sem6/CrocMyProject/MyProject/db/movies";
-
-    try (Connection conn = DriverManager.getConnection(url, "admin", "admin")) {
-        System.out.println("Установили соединение с БД");
-    } catch (SQLException e) {
-        System.err.println("Ошибка при работе с БД: " + e.getMessage());
+        try (Connection conn = DriverManager.getConnection(ConnectionConfig.URL, "admin", "admin")) {
+      String sql = "select * from users";
+      try (Statement statement = conn.createStatement()) {
+        boolean hasResult = statement.execute(sql);
+        if (hasResult) {
+          try (ResultSet resultSet = statement.getResultSet()) {
+            while (resultSet.next()) {
+              String username = resultSet.getString(2);
+              String format = String.format("%s", username);
+              System.out.println(format);
+            }
+          }
+        }
       }
+    } catch (SQLException e) {
+      System.err.println("Ошибка при работе с БД: " + e.getMessage());
+    }
   }
 }
